3fa6b1e4d0de8655d7404740d8cf0426b1da3e68
Jose Fernandez Garrido <34067926+Y0s3p@users.noreply.github.com>
2019-02-11 18:13:06 +0100

    Add files via upload

---
+++ SuperBarato/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Super Barato Alcala',
+    'summary': 'módulo tienda',
+    'author': 'Jose Fernandez Garrido',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': ['views/proveedores.xml', 'views/clientes.xml', 'views/articulos.xml']
+}
